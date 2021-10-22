b703464267b247d685b56e7873ba73349d96bc7f
42KIM <75300807+42KIM@users.noreply.github.com>
2021-10-23 03:11:45 +0900

    build: craco alias 설정
    * style: eslint rules 설정 변경
    
    * style: stylelint rules 설정 변경
    
    * chore: src/styles 디렉토리 alias 추가
    
    * chore: craco alias 설정 변경
    
    * chore: eslint rule 설정 변경

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,18 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+  webpackFinal: async (config) => {
+    config.resolve.alias['@components'] = path.resolve(
+      __dirname,
+      '../src/components',
+    );
+    config.resolve.alias['@hooks'] = path.resolve(__dirname, '../src/hooks');
+    return config;
+  },
+};
--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,20 @@
-const path = require('path');
+const CracoAlias = require('craco-alias');
 
 module.exports = {
-  webpack: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@contexts': path.resolve(__dirname, 'src/contexts'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'options',
+        baseUrl: './src',
+        aliases: {
+          '@components': '/components',
+          '@hooks': '/hooks',
+          '@contexts': '/contexts',
+          '@pages': '/pages',
+          '@styles': '/styles',
+        },
+      },
     },
-  },
+  ],
 };
