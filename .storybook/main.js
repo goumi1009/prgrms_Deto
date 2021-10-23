fc3a19b1d4c0c135fdb243700b4384cbcd372e3d
ksh <ksahoo524@gmail.com>
2021-10-24 02:29:06 +0900

    fix: storybook alias 동작을 위한 main.js 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,6 +13,12 @@ module.exports = {
       '../src/components',
     );
     config.resolve.alias['@hooks'] = path.resolve(__dirname, '../src/hooks');
+    config.resolve.alias['@contexts'] = path.resolve(
+      __dirname,
+      '../src/contexts',
+    );
+    config.resolve.alias['@pages'] = path.resolve(__dirname, '../src/pages');
+    config.resolve.alias['@styles'] = path.resolve(__dirname, '../src/styles');
     return config;
   },
 };
