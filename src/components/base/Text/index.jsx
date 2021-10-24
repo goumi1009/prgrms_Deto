95e10634985a17530fd9d142e1accb9da3dccd66
42KIM <75300807+42KIM@users.noreply.github.com>
2021-10-25 00:16:11 +0900

    feat: base/Text 컴포넌트 구현
    * feat: base/Text 컴포넌트 구현
    
    * style: 상대경로를 절대경로로 수정
    
    * style: fontType, color에 정의된 style 변수 적용
    
    * style: text 컴포넌트 prop 이름 변경

---
+++ src/components/base/Text/index.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import fontType from '@styles/fontType';
+import color from '@styles/color';
+
+const StyledText = styled.span`
+  ${(props) => fontType[props.fontType]}
+  font-weight: ${(props) => (props.strong ? 'bold' : undefined)};
+  color: ${(props) => color[props.color] || props.color};
+`;
+
+const Text = ({ content, fontType, strong, color }) => (
+  <StyledText fontType={fontType} strong={strong} color={color}>
+    {content}
+  </StyledText>
+);
+
+Text.defaultProps = {
+  content: 'Text를 입력해주세요',
+  fontType: 'fontBase',
+  strong: false,
+  color: 'primary',
+};
+
+Text.propTypes = {
+  content: PropTypes.string,
+  fontType: PropTypes.string,
+  strong: PropTypes.bool,
+  color: PropTypes.string,
+};
+
+export default Text;
---
+++ src/stories/Text.stories.jsx
@@ -0,0 +1,8 @@
+import Text from '@components/base/Text';
+
+export default {
+  title: 'Component/Text',
+  component: Text,
+};
+
+export const Default = (args) => <Text {...args} />;
