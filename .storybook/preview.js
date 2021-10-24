5697efc43045ffdfc46cace581defb3ce83fc8cd
goumi <goumi1009@gmail.com>
2021-10-25 01:57:25 +0900

    style: globalStyle normalize 스타일 추가
    storybook에 globalStyle 적용을 위해 preview에 decorators 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,21 @@
+import React from 'react';
+import GlobalStyle from '@styles/GlobalStyle';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <div>
+      <GlobalStyle />
+      <Story />
+    </div>
+  ),
+];
