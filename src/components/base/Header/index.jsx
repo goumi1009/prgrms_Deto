e73267ba4207d11e80a330846d0f8b62763cda91
goum <40739041+goumi1009@users.noreply.github.com>
2021-11-02 17:06:20 +0900

    refactor: header 스타일 변경, 메뉴 추가, 링크연결
    * feat: modal 컴포넌트 작업중
    
    * feat: modal 컴포넌트 구현
    
    * feat: header 컴포넌트 UI 구조 및  style 구현
    
    * feat: header컴포넌트 search input 열고 닫기 기능 추가
    
    * feat: 검색인풋 상태 추가
    
    * feat: stack 클릭시 modal창 뜨게 연결
    
    * feat: 페이지에 상태에 따라 버튼 조건부 렌더링 되도록 props 추가
    
    * build: react-router 라이브러리 추가
    
    * feat: react-router 페이지 초기 설정
    
    * fix: 링크버튼 router Link컴포넌트로 수정
    
    * fix: 라우터 추가로 인한 스토리북 오류 수정
    
    * fix: 모달 제거, 새포스트 링크 로그인상태에 따라 분기, isSearch => isMainPage 로 수정
    
    * refactor: header 버튼 스타일 변경 및 드롭다운 추가 각페이지 링크연결
    
    * style: button, link style component 수정
    
    Co-authored-by: goumi1009 <goumi1009@gamil.com>

--- src/components/base/Header/index.jsx
+++ src/components/base/Header/index.jsx
@@ -4,13 +4,16 @@ import { useAuthContext } from '@contexts/AuthProvider';
 import { Link, useLocation } from 'react-router-dom';
 import color from '@styles/color';
 import fontType from '@styles/fontType';
+import Icon from '@components/base/icon';
+import Text from '@components/base/Text';
+import ProfileBox from '@components/base/ProfileBox';
 
 const AppHeaderWrapper = styled.div`
   display: flex;
   width: 100%;
   margin: 0 auto;
   max-width: 1120px;
-  padding: 0 20px;
+  padding: 0 30px;
   height: 50px;
   align-items: center;
   border-bottom: 1px solid ${color.border};
@@ -23,20 +26,50 @@ const Logo = styled.h1`
 `;
 
 const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
   margin-left: auto;
 `;
 
+const IconButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  &:hover {
+    background-color: ${color.border};
+    border-radius: 50%;
+  }
+`;
+
+const IconLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  &:hover {
+    background-color: ${color.border};
+    border-radius: 50%;
+  }
+`;
+
 const SearchWrapper = styled.div`
-  display: inline-block;
+  display: flex;
+  height: 100%;
+  z-index: 10;
 `;
 
 const SearchForm = styled.form`
   display: ${({ searchOpen }) => (searchOpen ? 'flex' : 'none')};
+  align-items: center;
   position: absolute;
   left: 0;
   top: 0;
   width: 100%;
   height: 100%;
+  padding: 0 20px;
   background: ${color.white};
   input {
     flex-grow: 1;
@@ -45,11 +78,98 @@ const SearchForm = styled.form`
   }
 `;
 
+const DropMenu = styled.div`
+  display: ${({ showMenu }) => (showMenu ? 'block' : 'none')};
+  position: absolute;
+  right: 20px;
+  top: 45px;
+  padding: 16px;
+  background-color: ${color.white};
+  border-radius: 4px;
+
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.2);
+  z-index: 10;
+  opacity: ${({ showMenu }) => (showMenu ? 1 : 0)};
+  transition: all 0.3s ease-in 0.1s;
+
+  a,
+  button {
+    &:not(:last-child) {
+      margin-bottom: 8px;
+    }
+  }
+`;
+
+const ColorButton = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 140px;
+  height: 40px;
+  border-radius: 4px;
+  background: ${color.greenLight};
+
+  span {
+    display: block;
+    margin-left: 8px;
+  }
+
+  &:hover {
+    background-color: ${color.green};
+  }
+`;
+
+const BorderButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 140px;
+  height: 40px;
+  border-radius: 4px;
+  background-color: ${color.white};
+  border: 2px solid ${color.greenLight};
+
+  span {
+    display: block;
+    margin-left: 8px;
+  }
+
+  &:hover {
+    border-color: ${color.green};
+    span {
+      color: ${color.green};
+    }
+  }
+`;
+const BorderLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 140px;
+  height: 40px;
+  border-radius: 4px;
+  background-color: ${color.white};
+  border: 2px solid ${color.greenLight};
+
+  span {
+    display: block;
+    margin-left: 8px;
+  }
+
+  &:hover {
+    border-color: ${color.green};
+    span {
+      color: ${color.green};
+    }
+  }
+`;
+
 const Header = () => {
   const [searchOpen, setSearchOpen] = useState(false);
   const [value, setValue] = useState('');
-  const { isLoggedIn, handleLogout } = useAuthContext();
+  const { userToken, handleLogout, userInfo } = useAuthContext();
   const [isMainPage, setIsMainPage] = useState();
+  const [showMenu, setShowMenu] = useState(false);
   const location = useLocation();
 
   useEffect(
@@ -63,50 +183,110 @@ const Header = () => {
   };
 
   const onSearchClose = () => {
+    setValue('');
     setSearchOpen(false);
+  };
+
+  const closeMenu = () => {
+    setShowMenu(false);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     onSearchClose();
-    setValue('');
+  };
+
+  const handleLogoutClick = () => {
+    handleLogout();
+    closeMenu();
   };
 
   const handleChange = (e) => {
     const { value } = e.target;
     setValue(value);
   };
-
+  // console.log(userToken, userInfo);
   return (
     <AppHeaderWrapper>
-      <Link to="/">
-        <Logo>Deto</Logo>
-      </Link>
+      <Logo>
+        <Link to="/">Deto</Link>
+      </Logo>
       <ButtonWrapper>
-        <button type="button" onClick={handleLogout}>
-          (임시로그아웃버튼)
-        </button>
-        <Link to={isLoggedIn ? '/createPost' : '/login'}>새포스트</Link>
-        {isLoggedIn ? <Link to="/alarm">알림</Link> : undefined}
+        <IconLink to={userToken ? '/post/create' : '/login'}>
+          <Icon name="plus-square" color={color.secondary} />
+        </IconLink>
+        {userToken ? (
+          <IconLink to="/user/alarm">
+            <Icon name="bell" color={color.secondary} />
+          </IconLink>
+        ) : undefined}
         {isMainPage ? (
           <SearchWrapper>
-            <button type="button" onClick={onSearchOpen}>
-              검색
-            </button>
+            <IconButton type="button" onClick={onSearchOpen}>
+              <Icon name="search" color={color.secondary} />
+            </IconButton>
             <SearchForm onSubmit={handleSubmit} searchOpen={searchOpen}>
-              <button type="button" onClick={onSearchClose}>
-                닫기
-              </button>
+              <IconButton type="button" onClick={onSearchClose}>
+                <Icon name="arrow-left" color={color.secondary} />
+              </IconButton>
               <input onChange={handleChange} value={value} />
-              <button type="submit">검색</button>
+              <IconButton type="submit">
+                <Icon name="search" color={color.green} />
+              </IconButton>
             </SearchForm>
           </SearchWrapper>
         ) : undefined}
-
-        <button type="button">메뉴</button>
+        <IconButton
+          type="button"
+          onClick={() => {
+            setShowMenu(!showMenu);
+          }}
+        >
+          <Icon name="menu" color={color.green} />
+        </IconButton>
+        <DropMenu showMenu={showMenu}>
+          {userToken ? (
+            <>
+              <ColorButton to={`/user/${userInfo.userId}`} onClick={closeMenu}>
+                <ProfileBox
+                  size={24}
+                  src={userInfo.userProfile}
+                  content={userInfo.userName}
+                  color="white"
+                  strong
+                />
+              </ColorButton>
+              <BorderButton type="button" onClick={handleLogoutClick}>
+                <Icon
+                  name="log-out"
+                  color={color.greenLight}
+                  hoverColor={color.green}
+                />
+                <Text content="로그아웃" color="greenLight" strong />
+              </BorderButton>
+            </>
+          ) : (
+            <>
+              <BorderLink to="/user/login" onClick={closeMenu}>
+                <Icon
+                  name="log-in"
+                  color={color.greenLight}
+                  hoverColor={color.green}
+                />
+                <Text content="로그인" color="greenLight" strong />
+              </BorderLink>
+              <ColorButton to="/user/signup" onClick={closeMenu}>
+                <Icon name="user-plus" color={color.white} />
+                <Text content="회원가입" color="white" strong />
+              </ColorButton>
+            </>
+          )}
+        </DropMenu>
       </ButtonWrapper>
     </AppHeaderWrapper>
   );
 };
+
+// name, size, strokeWidth, color, rotate
 
 export default Header;
